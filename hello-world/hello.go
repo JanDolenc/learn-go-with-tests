be932763489532e288d087fc0cb83cd3afa2676e
Jan Dolenc <jan.dolenc9@gmail.com>
2025-07-13 18:57:47 +0200

    hello world v3
    Hello YOU
    
    Write test first, then implement the requirement. Requirement is to
    greet you.

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -6,11 +6,11 @@ import "fmt"
 // Why? To make it easier to test
 
 // Domain is the "Hello world!" string
-func Hello() string {
-	return "Hello, world!"
+func Hello(name string) string {
+	return "Hello, " + name + "!"
 }
 
 func main() {
 	// Println (printing to stdout) is a side effect
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
