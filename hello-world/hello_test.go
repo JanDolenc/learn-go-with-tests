916e8512d7b0f5af1a9da30a32004e371ff6a0c7
Jan Dolenc <jan.dolenc9@gmail.com>
2025-07-17 22:12:17 +0200

    hello world v6
    Refactor
    
    encapsulate the assertion into a function. This reduces the duplication
    and improves readability of the tests.

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -8,18 +8,18 @@ func TestHello(t *testing.T) {
 
 		got := Hello("Jan")
 		want := "Hello, Jan!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // tell test suite that assertCorrectMessage is a helper and it helps when tracking down the problems when test is failing - the line number reported is in our function call not in this helper func.
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
