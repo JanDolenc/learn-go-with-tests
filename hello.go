c6abf32b1ef84dee4ae441fb9efcc8bf39b79206
Jan Dolenc <jan.dolenc9@gmail.com>
2026-05-27 11:45:58 +0200

    hello world v3
    Refactor
    
    We just improved already working code - specify person to greet.

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Domain code - Hello() should be separated from side effects - printing to the stdout
 // Why? To make it easier to test our code.
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
