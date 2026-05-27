b9ba827cf5a03b2eaa5e385692529037508c809c
Jan Dolenc <jan.dolenc9@gmail.com>
2026-05-27 13:13:01 +0200

    hello world v6
    Specify language of the greeting
    
    - Add option to specify the greeting language.
    - First, implement this requirements into the test, then write code to
    make the test pass.

--- hello.go
+++ hello.go
@@ -6,13 +6,18 @@ const englishHelloPrefix = "Hello, "
 
 // Domain code - Hello() should be separated from side effects - printing to the stdout
 // Why? To make it easier to test our code.
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Manca"))
+	fmt.Println(Hello("Manca", ""))
 }
