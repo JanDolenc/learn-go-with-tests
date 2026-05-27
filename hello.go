70592af38ad032f1f613aef8cb8e0baf650560ca
Jan Dolenc <jan.dolenc9@gmail.com>
2026-05-27 11:43:05 +0200

    hello wolrd v2
    Hello YOU
    
    - Specify the person to greet.
    - Write the test first to express our requirements in test form.
    - Then update the code to make the test past.
    - At this point the code is working as we expect, but we will make some
    refactoring to improve it next commit.

--- hello.go
+++ hello.go
@@ -4,10 +4,10 @@ import "fmt"
 
 // Domain code - Hello() should be separated from side effects - printing to the stdout
 // Why? To make it easier to test our code.
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Manca"))
 }
