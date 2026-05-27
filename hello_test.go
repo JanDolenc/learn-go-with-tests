1f81fd7be3a3db4e347cb2a109adbab3668786f1
Jan Dolenc <jan.dolenc9@gmail.com>
2026-05-27 12:59:57 +0200

    hello world v5
    Refactor tests
    
    - Refactoring is not just for production code, it is for tests too.
    - Add helper function, to make the code more readable and avoid
    duplication.
    - Test refactor such as this one, should normally be amended into last
    commit, because we really only care about the lovely version of our code
    with the tests.

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,20 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Jan")
 		want := "Hello, Jan"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+// helper function - commonly accept testing.TB
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // tells testing suite that this method is a helper.
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
