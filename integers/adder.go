e678d252f18c9c220abf5710f703be1c4425c913
Jan Dolenc <jan.dolenc9@gmail.com>
2026-06-09 15:26:27 +0200

    add a package comment

--- integers/adder.go
+++ integers/adder.go
@@ -1,3 +1,8 @@
+// Package integers, explains how integers work in Go (as you would expect).
+// Adding a comment like this add the top of the file (or in doc.go for long
+// comments) serves as a documentation, that you can then use with `go doc package`
+// or pkgsite. If you don't have a comment like this, then golangci-lint displays a
+// warning and nudges you to follow Go's documentation best practices.
 package integers
 
 func Add(x, y int) int {
