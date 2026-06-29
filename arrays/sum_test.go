d2748fc3d4cc412dd320cdcf4197f08ff6319c5a
Jan Dolenc <jan.dolenc9@gmail.com>
2026-06-29 22:26:16 +0200

    arrays v6
    SumAll func
    - Add new function that sums up all slices passed in
    - Write test first, then more and more code to make it pass
    - Use variatic function that can take variable number of arguments
    - Use slices package to compare slices - you can't use = on slices
    - Use make keyword to make create a slice with starting capacity

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,16 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	n1 := []int{1, 2}
+	n2 := []int{0, 9}
+
+	got := SumAll(n1, n2)
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
