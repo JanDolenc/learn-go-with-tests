d7f6e12f20190345b9ec5d1670d7bbe1a62ec781
Jan Dolenc <jan.dolenc9@gmail.com>
2026-07-17 07:58:42 +0200

    structs v4
    Add support for calculating area of circles.
    
    - To be able to calculate area of rectangles and circles we are using
    structs and methods.
    - Define struct for each shape and "attach" a method Area() -
    rectangle.Area(), circle.Area().
    - This way we make sure that the "right" Area() is getting called and
    calculated depending on the shape - rectangle or circle.

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,15 @@
 package main
 
+import "math"
+
 // Rectangle has the dimensions of a rectangle.
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 // Perimeter returns the perimeter of a rectangle.
@@ -11,7 +17,11 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-// Area returns the area of a rectangle.
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+// Circle represents a circle...
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
